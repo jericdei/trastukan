3c98b34d6650d2f796692a4228058684b4ff34b6
Jeric June Logan <loganjeric25@gmail.com>
2024-05-07 22:53:58 +0800

    setup projects

---
+++ backend/index.ts
@@ -0,0 +1,20 @@
+const server = Bun.serve<{ authToken: string }>({
+  fetch(req, server) {
+    const success = server.upgrade(req);
+    if (success) {
+      return undefined;
+    }
+
+    return new Response("This is a WebSocket Server!");
+  },
+  websocket: {
+    // this is called when a message is received
+    async message(ws, message) {
+      console.log(`Received ${message}`);
+      // send back a message
+      ws.send(`You said: ${message}`);
+    },
+  },
+});
+
+console.log(`Listening on ${server.hostname}:${server.port}`);
